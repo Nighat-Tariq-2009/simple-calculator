ee635d8248f93a71c8a36f6eb9003ac33a804953
Nighat-Tariq-2009 <nighattariq1986@gmail.com>
2024-03-23 01:16:47 +0500

    calculator

---
+++ main.js
@@ -0,0 +1,5 @@
+//BMI calculator in typescript
+var weight = 70;
+var height = 5.4;
+var bmi = weight / height * height;
+console.log("Night your BMI Is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,7 @@
+//BMI calculator in typescript
+
+let weight = 70;
+let height = 5.4;
+
+let bmi = weight/height*height
+console.log(`Night your BMI Is ${bmi}`);
